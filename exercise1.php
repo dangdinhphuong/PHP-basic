c2337feebb15b0ee0116f7f91c64c3da06baaeb7
phuong <dangxuong2000@gmail.com>
2021-08-11 16:01:36 +0700

    update

--- exercise1.php
+++ exercise1.php
@@ -1,29 +1,29 @@
+<?php
+$readFile1 = "file1.txt";
+$readFile2 = "file2.txt";
+function checkValidString($getString, $notContain, $contain)
+{
+    if (strstr($getString, $notContain) == false && strstr($getString, $contain) == true) {
+        echo "Chuỗi  hợp lệ";
 
-  <?php
-    $readFile1 = "file1.txt";
-    $readFile2 = "file2.txt";
-    function checkValidString($getString, $notContain, $contain)
-    {
-        if (strstr($getString, $notContain) == false && strstr($getString, $contain) == true) {
-            echo "Chuỗi  hợp lệ";
-            return true;
-        } else {
-            echo "Chuỗi  không hợp lệ";
-            return false;
-        }
+        return true;
+    } else {
+        echo "Chuỗi  không hợp lệ";
+
+        return false;
     }
+}
 
-
-    function getCheckfile($getString, $notContain, $contain)
-    {
-        if (checkValidString($getString, $notContain, $contain) == true) {
-            echo "Chuỗi  hợp lệ. chuỗi bao gồm " . count(explode('.', $getString)) . " câu.";
-        } else {
-            echo "Chuỗi  không hợp lệ";
-        }
+function getCheckfile($getString, $notContain, $contain)
+{
+    if (checkValidString($getString, $notContain, $contain) == true) {
+        echo "Chuỗi  hợp lệ. chuỗi bao gồm " . count(explode('.', $getString)) . " câu.";
+    } else {
+        echo "Chuỗi  không hợp lệ";
     }
+}
 
-
-    getCheckfile(file_get_contents($readFile1), "restaurant", "Lorem");
-    checkValidString(file_get_contents($readFile1), "restaurant", "Lorem");
-    ?>
+// lesson 1
+checkValidString(file_get_contents($readFile1), "restaurant", "Lorem");
+// lesson 2
+getCheckfile(file_get_contents($readFile1), "restaurant", "Lorem");
