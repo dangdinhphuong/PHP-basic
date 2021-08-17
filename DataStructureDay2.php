dd10c47ec633ae3170a2cd38814d7bf30e2a7367
phuong <dangxuong2000@gmail.com>
2021-08-17 23:13:17 +0700

    the second time

--- DataStructureDay2.php
+++ DataStructureDay2.php
@@ -1,6 +1,4 @@
-<?php
-
-
+<?phP
 $price = 210;
 $rivalryPrice = 1200;
 $stepPrices = [
@@ -20,60 +18,67 @@ $calendar = [
     true,  // tương đương với ngày 6
     true, // tương đương với ngày 7
 ];
-
+$count = 0;
+$number_of_days = 0;
+$deadline = 0;
 
 function calcMinPrice($price, $stepPrices, $rivalryPrice)
 {
     $a = 0;
     $b = 0;
-    while ($a <= $rivalryPrice) {
-        if ($stepPrices[100] <= $price && $price < 300) {
-            $a = $price + $stepPrices[100];
-        } else if (300 <= $price && $price < 500) {
-            $a = $price + $stepPrices[300];
-        } else if (500 <= $price && $price < 1000) {
-            $a = $price + $stepPrices[500];
-        } else if (1000 <= $price && $price < 1500) {
-            $a = $price + $stepPrices[1000];
-        } else if (1500 <= $price) {
-            $a = $price + $stepPrices[15000];
-        }
 
+    if (100 <= $price && $price < 300) {
+        $a = $price + $stepPrices[100];
+    } else if (300 <= $price && $price < 500) {
+        $a = $price + $stepPrices[300];
+    } else if (500 <= $price && $price < 1000) {
+        $a = $price + $stepPrices[500];
+    } else if (1000 <= $price && $price < 1500) {
+        $a = $price + $stepPrices[1000];
+    } else if (1500 <= $price) {
+        $a = $price + $stepPrices[1500];
+    }
 
-        if ($rivalryPrice >= $a) {
-            if (100 <= $a && $a < 300) {
-                $b = $a + $stepPrices[100];
-                $price = $b;
-            } else if (300 <= $a && $a < 500) {
-                $b = $a + $stepPrices[300];
-                $price = $b;
-            } else if (500 <= $a && $a < 1000) {
-                $b = $a + $stepPrices[500];
-                $price = $b;
-            } else if (1000 <= $a && $a < 1500) {
-                $b = $a + $stepPrices[1000];
-                $price = $b;
-            }
-            echo $a . '=' . $b;
-            echo "<br>";
+    if ($rivalryPrice >= $a) {
+        if (100 <= $a && $a < 300) {
+            $b = $a + $stepPrices[100];
+            $price = $b;
+        } else if (300 <= $a && $a < 500) {
+            $b = $a + $stepPrices[300];
+            $price = $b;
+        } else if (500 <= $a && $a < 1000) {
+            $b = $a + $stepPrices[500];
+            $price = $b;
+        } else if (1000 <= $a && $a < 1500) {
+            $b = $a + $stepPrices[1000];
+            $price = $b;
         }
+        echo $a . '=' . $b;
+        echo "<br>";
+        calcMinPrice($price, $stepPrices, $rivalryPrice);
     }
 }
+
+
 function calcDeadline($manday, $calendar)
 {
-    $count = 0;
-    $deadline = 0;
-    for ($i = 0; $i < count($calendar); $i++) {
-        if ($calendar[$i] === false) {
-            $count++;
-            $deadline = $i;
-        }
+    global $count,$deadline,$number_of_days;
+    if($manday>count($calendar)){
+        return false;
     }
-    
-    echo $deadline . "===" . $count;
+    if (isset($calendar[$count])) {
+        if ($calendar[$count] === false) {
+            $number_of_days++;
+            $deadline = $count;
+        }
+        $count += 1;
+        calcDeadline($manday, $calendar);
+    }
+
+     return "Hạn deadline ngày thứ: ".$deadline . "<br> Số ngày làm việc: " . $number_of_days;
 }
 //## Bài toán đấu giá
 calcMinPrice(210, $stepPrices, 1200);
 
 //## Bài dự tính deadline
-calcDeadline(5, $calendar);
+echo(calcDeadline(5, $calendar));
